d97a7ec59a5f17b3dd0396ac2850ebbe9679a853
harikanth87 <141509036+harikanth87@users.noreply.github.com>
2026-02-16 15:43:25 +0530

    SUM add two num

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+
+    int a=5;
+    int b=10;
+    int sum = a+b;
+    System.out.println("sum is: " + sum);
+    
+        
     }
 }
